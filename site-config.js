1b71a0c1e11f85b9e2d3961751d02e4abf7375ca
HydeChan9 <93176135+HydeChan9@users.noreply.github.com>
2026-05-19 14:16:07 +1000

    clear old website

--- site-config.js
+++ site-config.js
@@ -1,7 +1,8 @@
 window.FORGEKEYS_CONFIG = {
-  submissionMode: "draft",
+  submissionMode: "supabase",
   submissionEndpoint: "",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  supabaseBucket: "design-submissions"
+  supabaseUrl: "https://fuyfhvcplchzxkcwfaxt.supabase.co/rest/v1/",
+  supabaseAnonKey: "sb_publishable_8qFQLJ7XT_AshCHB3rU3Fw_PIzJ2115",
+  supabaseBucket: "design-submissions",
+  supabaseFolder: "submissions"
 };
